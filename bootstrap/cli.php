06659b1d44558929999e2510e7314148a5ddc4e4
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-08-23 10:35:12 +0600

    refactor: add CLI runtime bootstrap

---
+++ bootstrap/cli.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+use Infocyph\Foundation\Foundation;
+
+/** @var array<string, mixed> $options */
+$options = require __DIR__ . '/options.php';
+
+return Foundation::cli($options);
