48fed569f3a621fae5c3829f93786681c4761854
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-08-23 10:40:40 +0600

    refactor: simplify web bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Infocyph\Foundation\Foundation;
 
-/** @var array<string, mixed> $options */
-$options = require __DIR__ . '/options.php';
-
-return Foundation::web($options);
+return Foundation::web([
+    'base_path' => dirname(__DIR__),
+]);
